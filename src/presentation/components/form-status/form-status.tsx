4b9fdfa87feedb36b2b8d1a345c793757672b0c5
Victor Lima <victor.lima@alicredito.com.br>
2020-07-19 20:55:41 -0300

    feat: add app router

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Styles from './form-status-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
 
-
 const FormStatus: React.FC = () => {
-    return (
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>
+  return (
+    <div className={Styles.errorWrap}>
+      <Spinner className={Styles.spinner} />
+      <span className={Styles.error}>
             Erro
-          </span>
-        </div>
-    )
+      </span>
+    </div>
+  )
 }
 
 export default FormStatus
