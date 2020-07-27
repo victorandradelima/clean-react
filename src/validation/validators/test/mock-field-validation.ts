997883430be2bce808683bba5289bf068aa8b40b
Victor Lima <victor.lima@alicredito.com.br>
2020-07-27 11:15:27 -0300

    feat: ensure ValidationComposite return error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class FieldValidationSpy implements FieldValidation {
+  constructor (readonly field: string) {}
+  error: Error = null
+  validate (value: string): Error {
+    return this.error
+  }
+}
