16e85330fa0637e9a80d097d4299674b63800ad3
Victor Lima <victor.lima@alicredito.com.br>
2020-07-27 18:20:21 -0300

    test: ensure ValidationBuilder a list of validations

--- src/validation/builder/validation-buider.spec.ts
+++ src/validation/builder/validation-buider.spec.ts
@@ -1,19 +1,35 @@
 import { RequiredFieldValidation, MinLengthValidation, EmailValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-buider'
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmaildValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const field = faker.database.column()
+    const validations = sut.field(field).email().build()
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthdValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    const field = faker.database.column()
+    const length = faker.random.number()
+    const validations = sut.field(field).min(length).build()
+    expect(validations).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('Should return a list of validation', () => {
+    const field = faker.database.column()
+    const length = faker.random.number()
+    const validations = sut.field(field).required().email().min(length).build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthValidation(field, length)
+    ])
   })
 })
