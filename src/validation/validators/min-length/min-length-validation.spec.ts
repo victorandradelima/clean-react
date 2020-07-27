f1974e99f6db2de47b11813ccf828c5ba8e781ef
Victor Lima <victor.lima@alicredito.com.br>
2020-07-27 10:21:37 -0300

    test: ensure MinLengthValidation returns falsy is value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,11 +1,16 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-validation'
 import { MinLengthValidation } from './min-length-validation'
 
-
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
     const sut = new MinLengthValidation('field', 5)
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy is value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
   })
 })
